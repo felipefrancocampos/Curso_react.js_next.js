a0718856dd5602b8bda01ab99949080930ae9a43
Felipe Franco Campos <felipe.francocampos024@gmail.com>
2021-03-23 06:39:20 -0300

    .

--- project_01/src/component/PostCard/index.jsx
+++
@@ -1,9 +0,0 @@
-export const PostCard = ({ title, cover, body, id }) => (
-    <div className="post">
-        <img src={cover} alt= {title} />
-        <div key={id} className="post-content">
-            <h1>{title}</h1>
-            <p>{body}</p>
-        </div>
-    </div>
-    )
