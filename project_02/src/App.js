78de7518543061ec59b85691dd6a97e6a1815a45
Felipe Franco Campos <felipe.francocampos024@gmail.com>
2021-05-11 22:46:57 -0300

    .

--- project_02/src/App.js
+++ project_02/src/App.js
@@ -1,16 +1,18 @@
-import P from 'prop-types'
-import { useEffect, useMemo, useState } from 'react';
+import P from 'prop-types';
+import { useEffect, useMemo, useState, useRef } from 'react';
 import './App.css';
 
-const Post = ({ post, onClick }) => {
+const Post = ({ post, handleClick }) => {
   console.log('Filho renderizou');
   return (
-    <div key={post.id} className="posts">
-      <h1>{post.title}</h1>
+    <div key={post.id} className="post">
+      <h1 style={{ fontSize: '14px' }} onClick={() => handleClick(post.title)}>
+        {post.title}
+      </h1>
       <p>{post.body}</p>
     </div>
   );
-}
+};
 
 Post.propTypes = {
   post: P.shape({
@@ -18,30 +20,43 @@ Post.propTypes = {
     title: P.string,
     body: P.string,
   }),
-  onClick: P.func,
+  handleClick: P.func,
 };
 
 function App() {
   const [posts, setPosts] = useState([]);
   const [value, setValue] = useState('');
+  const input = useRef(null);
+  const contador = useRef(0);
 
-  console.log('Pai, renderizou!');
+  console.log('Pai renderizou!');
 
-  //component did mount
+  // Component did mount
   useEffect(() => {
-      fetch('https:jsonplaceholder.typicode.com/posts')
-        .then((r) => r.json())
-        .then((r) => setPosts(r))
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then((r) => r.json())
+      .then((r) => setPosts(r));
   }, []);
 
-  const handleClick = () => {
+  useEffect(() => {
+    input.current.focus();
+    console.log(input.current);
+  }, [value]);
 
-  }
+  useEffect(() => {
+    contador.current++;
+  });
+
+  const handleClick = (value) => {
+    setValue(value);
+  };
 
   return (
     <div className="App">
+      <h6>Renderizou: {contador.current}x</h6>
       <p>
         <input
+          ref={input}
           type="search"
           value={value}
           onChange={(e) => setValue(e.target.value)}
@@ -51,11 +66,11 @@ function App() {
         return (
           posts.length > 0 &&
           posts.map((post) => {
-            return <Post key={post.id} post={post} onClick={handleClick} />;
+            return <Post key={post.id} post={post} handleClick={handleClick} />;
           })
         );
       }, [posts])}
-      {posts.length <= 0 && <p>Ainda não existem posts</p>}
+      {posts.length <= 0 && <p>Ainda não existem posts.</p>}
     </div>
   );
 }
