8a03750e938c60cbd9b1e1ab16c2ea51a50c0571
Felipe Franco Campos <felipe.francocampos024@gmail.com>
2021-06-02 04:05:41 -0300

    .

--- project_02/src/components/Posts/index.jsx
+++ project_02/src/components/Posts/index.jsx
@@ -1,11 +1,20 @@
 import { useContext, useEffect, useRef } from 'react';
+import {
+  decrementCounter,
+  incrementCounter,
+} from '../../contexts/CounterProvider/action';
+import { CounterContext } from '../../contexts/CounterProvider/context';
 import { loadPosts } from '../../contexts/PostsProvider/actions';
 import { PostsContext } from '../../contexts/PostsProvider/context';
 
 export const Posts = () => {
   const isMounted = useRef(true);
+
   const postsContext = useContext(PostsContext);
   const { postsState, postsDispatch } = postsContext;
+
+  const counterContext = useContext(CounterContext);
+  const { counterState, counterDispatch } = counterContext;
 
   useEffect(() => {
     loadPosts(postsDispatch).then((dispatch) => {
@@ -13,7 +22,6 @@ export const Posts = () => {
         dispatch();
       }
     });
-
     return () => {
       isMounted.current = false;
     };
@@ -21,13 +29,18 @@ export const Posts = () => {
 
   return (
     <div>
+      <button onClick={() => incrementCounter(counterDispatch)}>
+        Counter {counterState.counter}+
+      </button>
+      <button onClick={() => decrementCounter(counterDispatch)}>
+        Counter {counterState.counter}-
+      </button>
       <h1>POSTS</h1>
       {postsState.loading && (
         <p>
           <strong>Carregando posts...</strong>
         </p>
       )}
-
       {postsState.posts.map((p) => (
         <p key={p.id}>{p.title}</p>
       ))}
