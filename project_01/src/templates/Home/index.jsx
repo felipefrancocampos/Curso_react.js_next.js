309bb8334a361419bfcf305bdce9b19dcf68a899
Felipe Franco Campos <felipe.francocampos024@gmail.com>
2021-06-09 03:17:36 -0300

    .

--- project_01/src/templates/Home/index.jsx
+++ project_01/src/templates/Home/index.jsx
@@ -1,23 +1,7 @@
-import React, { Suspense, useState } from 'react';
-// import LazyComponent from './lazy-component';
-
-const loadComponent = () => {
-  console.log('Componente carregando...');
-  return import('./lazy-component');
-};
-const LazyComponent = React.lazy(loadComponent);
-
 export const Home = () => {
-  const [show, setShow] = useState(false);
-
   return (
     <div>
-      <p>
-        <button onMouseOver={loadComponent} onClick={() => setShow((s) => !s)}>
-          Show {show ? 'LC on screen' : 'LC is off screen'}
-        </button>
-      </p>
-      <Suspense fallback={<p>Carregando...</p>}>{show && <LazyComponent />}</Suspense>
+      <p>Oi</p>
     </div>
   );
 };
