5c09892a8a5e51fd820b47c81fce8519323cc5b8
Felipe Franco Campos <felipe.francocampos024@gmail.com>
2021-06-10 00:44:53 -0300

    75

--- project_02/src/index.jsx
+++ project_02/src/index.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { Menu } from './components/Menu';
 import './index.css';
-import App from './templates/App';
+import { Abc } from './templates/Abc';
+import { App } from './templates/App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Menu />
+      <Switch>
+        <Route path="/" component={App} exact />
+        <Route path="/abc" component={Abc} exact />
+      </Switch>
+    </BrowserRouter>
+
   </React.StrictMode>,
   document.getElementById('root')
 );
