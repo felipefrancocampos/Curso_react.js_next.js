4659e6fe46cc19da525d0ba2f98017560dde2c40
Felipe Franco Campos <felipe.francocampos024@gmail.com>
2021-06-10 01:56:12 -0300

    77

--- project_02/src/templates/App/index.jsx
+++ project_02/src/templates/App/index.jsx
@@ -3,8 +3,18 @@ import './styles.css';
 import { PostsProvider } from '../../contexts/PostsProvider';
 import { Posts } from '../../components/Posts'
 import { CounterProvider } from '../../contexts/CounterProvider'
+import { useHistory } from 'react-router-dom/cjs/react-router-dom';
+import { useEffect } from 'react';
 
 export function App() {
+  const history = useHistory();
+
+  useEffect(() => {
+    setTimeout(function () {
+      history.push('/');
+    }, 5000);
+  }, [history]);
+
   return (
     <CounterProvider>
       <PostsProvider>
