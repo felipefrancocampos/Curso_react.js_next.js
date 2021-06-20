e7c2474866dd2d1673fca15f9f377873a8d29df1
Felipe Franco Campos <felipe.francocampos024@gmail.com>
2021-06-20 00:45:17 -0300

    .

---
+++ strapi_landing_pages/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'd441ed6dbd2103330d6a81c018373c15'),
+    },
+  },
+});
