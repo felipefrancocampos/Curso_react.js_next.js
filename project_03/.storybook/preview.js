61d239cd4ac7fc10da9a44379d5eb8b486a268c8
Felipe Franco Campos <felipe.francocampos024@gmail.com>
2021-07-16 00:38:48 -0300

    101

--- project_03/.storybook/preview.js
+++ project_03/.storybook/preview.js
@@ -4,6 +4,19 @@ import { theme } from '../src/styles/theme'
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
+  backgrounds: {
+    default: 'light',
+    values: [
+      {
+        name: 'light',
+        value: theme.colors.white,
+      },
+      {
+        name: 'dark',
+        value: theme.colors.primaryColor,
+      },
+    ]
+  }
 }
 
 export const decorators = [
@@ -14,3 +27,18 @@ export const decorators = [
     </ThemeProvider>
   )
 ];
+ 5  src/components/Heading/index.jsx
+@@ -1,10 +1,11 @@
+import P from 'prop-types';
+import * as Styled from './styles';
+
+export const Heading = ({ children }) => {
+  return <Styled.Title>{children}</Styled.Title>;
+export const Heading = ({ children, light = false }) => {
+  return <Styled.Title light={light}>{children}</Styled.Title>;
+};
+
+Heading.propTypes = {
+  children: P.node.isRequired,
+  light: P.bool.isRequired,
+};
