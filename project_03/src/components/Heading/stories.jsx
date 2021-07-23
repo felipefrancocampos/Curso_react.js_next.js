dd9fa6111c9c259d9ff12ef8e826371a0f93bcca
Felipe Franco Campos <felipe.francocampos024@gmail.com>
2021-07-23 17:50:22 -0300

    102

--- project_03/src/components/Heading/stories.jsx
+++ project_03/src/components/Heading/stories.jsx
@@ -1,14 +1,13 @@
 import { Heading } from '.';
+
 export default {
   title: 'Heading',
   component: Heading,
   args: {
     children: 'O texto está escuro',
-    light: false,
   },
   argTypes: {
     children: { type: 'string' },
-    light: { type: 'boolean' },
   },
   parameters: {
     backgrounds: {
@@ -18,7 +17,7 @@ export default {
 };
 
 export const Light = (args) => <Heading {...args} />;
-export const Dark = (args) => <Heading {...args} light={true} />;
+export const Dark = (args) => <Heading {...args} />;
 
 Light.parameters = {
   backgrounds: {
@@ -28,5 +27,5 @@ Light.parameters = {
 
 Dark.args = {
   children: 'O texto está claro',
-  light: true,
+  colorDark: false,
 };
