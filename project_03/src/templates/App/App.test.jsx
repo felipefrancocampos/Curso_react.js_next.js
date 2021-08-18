3db84a535cb004d681ab0e2bc6715c1ab423b9af
Felipe Franco Campos <felipe.francocampos024@gmail.com>
2021-08-18 20:18:08 -0300

    .

--- project_03/src/templates/App/App.test.jsx
+++ project_03/src/templates/App/App.test.jsx
@@ -3,15 +3,8 @@ import { renderTheme } from '../../styles/render-theme';
 import Home from '.';
 import { theme } from '../../styles/theme';
 
-test('renders learn react link', () => {
-  const { debug } = renderTheme(<Home />);
-  // eslint-disable-next-line prettier/prettier
-  const headingContainer = screen.getByRole('heading', { name: 'Hello' })
-    // eslint-disable-next-line prettier/prettier
-    .parentElement;
-  expect(headingContainer).toHaveStyle({
-    background: theme.colors.mainBg,
+describe('<Home />', () => {
+  it('should render home', () => {
+    renderTheme(<Home />);
   });
-  expect(headingContainer).toMatchSnapshot();
-  expect(headingContainer).toHaveStyleRule('background', 'red');
 });
