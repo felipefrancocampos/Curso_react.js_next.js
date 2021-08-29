04577034c64e0b884f0439d079cb0f69ef1d5406
Felipe Franco Campos <felipe.francocampos024@gmail.com>
2021-08-29 15:41:23 -0300

    110

--- project_03/src/components/Heading/TextComponent/TextComponent.test.jsx
+++ project_03/src/components/Heading/TextComponent/TextComponent.test.jsx
@@ -3,8 +3,13 @@ import { renderTheme } from '../../styles/render-theme';
 import { TextComponent } from '.';
 
 describe('<TextComponent />', () => {
-  it('should render', () => {
+  it('should render a Text', () => {
     renderTheme(<TextComponent>Children</TextComponent>);
-    expect(screen.getByRole('heading')).toBeInTheDocument();
+    expect(screen.getByText('children')).toBeInTheDocument();
+  });
+
+  it('should match snapshot', () => {
+    const { container } = renderTheme(<TextComponent>Children</TextComponent>);
+    expect(container.firstChild).toMatchInlineSnapshot();
   });
 });
