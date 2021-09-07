f61a2b55ee0e0558f1e2e84723f6eb5da5ee861f
Felipe Franco Campos <felipe.francocampos024@gmail.com>
2021-09-07 20:51:43 -0300

    115

---
+++ project_03/src/components/LogoLink/stories.jsx
@@ -0,0 +1,30 @@
+import { LogoLink } from '.';
+export default {
+  title: 'LogoLink',
+  component: LogoLink,
+  args: {
+    text: 'LogoLink',
+    srcImg: 'assets/images/logo.svg',
+    link: 'http://localhost',
+  },
+};
+
+export const ImageOnly = (args) => {
+  return (
+    <div>
+      <LogoLink {...args} />
+    </div>
+  );
+};
+
+export const TextOnly = (args) => {
+  return (
+    <div>
+      <LogoLink {...args} />
+    </div>
+  );
+};
+
+TextOnly.args = {
+  srcImg: '',
+};
