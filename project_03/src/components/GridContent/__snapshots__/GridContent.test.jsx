1ee96c083e93ad675ee2ae05f0ae08a8b586f1fa
Felipe Franco Campos <felipe.francocampos024@gmail.com>
2021-09-21 20:48:11 -0300

    .

--- project_03/src/components/GridContent/__snapshots__/GridContent.test.jsx
+++ project_03/src/components/GridContent/__snapshots__/GridContent.test.jsx
@@ -1,18 +1,15 @@
 // Jest Snapshot v1, https://goo.gl/fbAQLP
-
 exports[`<GridContent /> should render grid content 1`] = `
 .c3 {
   color: #0A1128;
   font-size: 6.4rem;
   text-transform: uppercase;
 }
-
 .c1 {
   max-width: 120rem;
   margin: 0 auto;
   padding: 3.2rem;
 }
-
 .c0 {
   background: #FFFFFF;
   color: #0A1128;
@@ -26,27 +23,22 @@ exports[`<GridContent /> should render grid content 1`] = `
   -ms-flex-align: center;
   align-items: center;
 }
-
 .c5 {
   font-size: 2.4rem;
 }
-
 .c2 {
   text-align: center;
   max-width: 58rem;
   margin: 0 auto;
 }
-
 .c4 {
   margin: 6.4rem 0;
 }
-
 @media (max-width:768px) {
   .c3 {
     font-size: 4.0rem;
   }
 }
-
 <div>
   <div
     class="c0"
@@ -57,126 +49,117 @@ exports[`<GridContent /> should render grid content 1`] = `
       <div
         class="c2"
       >
-        <h1
+        <h2
           class="c3"
         >
           O título que eu quero
-        </h1>
+        </h2>
         <div
           class="c4"
         >
-          <p
-            class="c5"
+        <div
+        class="c5"
+      >
+        <p>
+          <strong>
+            Pellentesque habitant morbi tristique
+          </strong>
+           senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper.
+          <em>
+            Aenean ultricies mi vitae est.
+          </em>
+           Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed,
+          <code>
+            commodo vitae
+          </code>
+          , ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui.
+          <a
+            href="#"
           >
-            <p>
-              <strong>
-                Pellentesque habitant morbi tristique
-              </strong>
-               senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper.
-              <em>
-                Aenean ultricies mi vitae est.
-              </em>
-               Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed,
-              <code>
-                commodo vitae
-              </code>
-              , ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui.
-              <a
-                href="#"
-              >
-                Donec non enim
-              </a>
-               in turpis pulvinar facilisis. Ut felis.
-            </p>
+            Donec non enim
+          </a>
+           in turpis pulvinar facilisis. Ut felis.
+        </p>
 
 
-
-            <h2>
-              Header Level 2
-            </h2>
-
+        <h2>
+          Header Level 2
+        </h2>
 
 
-            <ol>
+        <ol>
 
 
-              <li>
-                Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
-              </li>
+          <li>
+            Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+          </li>
 
 
-              <li>
-                Aliquam tincidunt mauris eu risus.
-              </li>
+          <li>
+            Aliquam tincidunt mauris eu risus.
+          </li>
 
 
-            </ol>
+        </ol>
 
 
-
-            <blockquote>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus magna. Cras in mi at felis aliquet congue. Ut a est eget ligula molestie gravida. Curabitur massa. Donec eleifend, libero at sagittis mollis, tellus est malesuada tellus, at luctus turpis elit sit amet quam. Vivamus pretium ornare est.
-              </p>
-            </blockquote>
-
-
-
-            <h3>
-              Header Level 3
-            </h3>
-
-
-
-            <ul>
-
-
-              <li>
-                Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
-              </li>
-
-
-              <li>
-                Aliquam tincidunt mauris eu risus.
-              </li>
-
-
-            </ul>
-
-
-
-            <pre>
-              <code>
-
-  #header h1 a {
-    display: block;
-    width: 300px;
-    height: 80px;
-  }
-
-              </code>
-            </pre>
+        <blockquote>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus magna. Cras in mi at felis aliquet congue. Ut a est eget ligula molestie gravida. Curabitur massa. Donec eleifend, libero at sagittis mollis, tellus est malesuada tellus, at luctus turpis elit sit amet quam. Vivamus pretium ornare est.
           </p>
+        </blockquote>
+
+
+        <h3>
+          Header Level 3
+        </h3>
+
+
+        <ul>
+
+
+          <li>
+            Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+          </li>
+
+
+          <li>
+            Aliquam tincidunt mauris eu risus.
+          </li>
+
+
+        </ul>
+
+
+        <pre>
+          <code>
+
+#header h1 a {
+display: block;
+width: 300px;
+height: 80px;
+}
+
+          </code>
+        </pre>
+        </div>
         </div>
       </div>
     </div>
   </div>
 </div>
 `;
-
 exports[`<GridContent /> should render grid content 2`] = `
 .c3 {
   color: #0A1128;
   font-size: 6.4rem;
   text-transform: uppercase;
 }
-
 .c1 {
   max-width: 120rem;
   margin: 0 auto;
   padding: 3.2rem;
 }
-
 .c0 {
   background: #FFFFFF;
   color: #0A1128;
@@ -190,27 +173,22 @@ exports[`<GridContent /> should render grid content 2`] = `
   -ms-flex-align: center;
   align-items: center;
 }
-
 .c5 {
   font-size: 2.4rem;
 }
-
 .c2 {
   text-align: center;
   max-width: 58rem;
   margin: 0 auto;
 }
-
 .c4 {
   margin: 6.4rem 0;
 }
-
 @media (max-width:768px) {
   .c3 {
     font-size: 4.0rem;
   }
 }
-
 <div>
   <div
     class="c0"
@@ -221,106 +199,100 @@ exports[`<GridContent /> should render grid content 2`] = `
       <div
         class="c2"
       >
-        <h1
+        <h2
           class="c3"
         >
           O título que eu quero
-        </h1>
+        </h2>
         <div
           class="c4"
         >
-          <p
-            class="c5"
+        <div
+        class="c5"
+      >
+        <p>
+          <strong>
+            Pellentesque habitant morbi tristique
+          </strong>
+           senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper.
+          <em>
+            Aenean ultricies mi vitae est.
+          </em>
+           Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed,
+          <code>
+            commodo vitae
+          </code>
+          , ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui.
+          <a
+            href="#"
           >
-            <p>
-              <strong>
-                Pellentesque habitant morbi tristique
-              </strong>
-               senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper.
-              <em>
-                Aenean ultricies mi vitae est.
-              </em>
-               Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed,
-              <code>
-                commodo vitae
-              </code>
-              , ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui.
-              <a
-                href="#"
-              >
-                Donec non enim
-              </a>
-               in turpis pulvinar facilisis. Ut felis.
-            </p>
+            Donec non enim
+          </a>
+           in turpis pulvinar facilisis. Ut felis.
+        </p>
 
 
-
-            <h2>
-              Header Level 2
-            </h2>
-
+        <h2>
+          Header Level 2
+        </h2>
 
 
-            <ol>
+        <ol>
 
 
-              <li>
-                Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
-              </li>
+          <li>
+            Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+          </li>
 
 
-              <li>
-                Aliquam tincidunt mauris eu risus.
-              </li>
+          <li>
+            Aliquam tincidunt mauris eu risus.
+          </li>
 
 
-            </ol>
+        </ol>
 
 
-
-            <blockquote>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus magna. Cras in mi at felis aliquet congue. Ut a est eget ligula molestie gravida. Curabitur massa. Donec eleifend, libero at sagittis mollis, tellus est malesuada tellus, at luctus turpis elit sit amet quam. Vivamus pretium ornare est.
-              </p>
-            </blockquote>
-
-
-
-            <h3>
-              Header Level 3
-            </h3>
-
-
-
-            <ul>
-
-
-              <li>
-                Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
-              </li>
-
-
-              <li>
-                Aliquam tincidunt mauris eu risus.
-              </li>
-
-
-            </ul>
-
-
-
-            <pre>
-              <code>
-
-  #header h1 a {
-    display: block;
-    width: 300px;
-    height: 80px;
-  }
-
-              </code>
-            </pre>
+        <blockquote>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus magna. Cras in mi at felis aliquet congue. Ut a est eget ligula molestie gravida. Curabitur massa. Donec eleifend, libero at sagittis mollis, tellus est malesuada tellus, at luctus turpis elit sit amet quam. Vivamus pretium ornare est.
           </p>
+        </blockquote>
+
+
+        <h3>
+          Header Level 3
+        </h3>
+
+
+        <ul>
+
+
+          <li>
+            Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+          </li>
+
+
+          <li>
+            Aliquam tincidunt mauris eu risus.
+          </li>
+
+
+        </ul>
+
+
+        <pre>
+          <code>
+
+#header h1 a {
+display: block;
+width: 300px;
+height: 80px;
+}
+
+          </code>
+        </pre>
+        </div>
         </div>
       </div>
     </div>
